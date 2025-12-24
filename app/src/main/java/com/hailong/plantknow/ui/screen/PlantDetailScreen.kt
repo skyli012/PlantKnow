55e2ac2e96cda8333b0237476363243cf349a0d9
sky_li <3367394861@qq.com>
2025-12-24 21:51:14 +0800

    upload and display pictures

--- app/src/main/java/com/hailong/plantknow/ui/screen/PlantDetailScreen.kt
+++ app/src/main/java/com/hailong/plantknow/ui/screen/PlantDetailScreen.kt
@@ -377,7 +377,7 @@ fun ScanAnotherPlantButton(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(top = 16.dp, start = 16.dp, end = 16.dp)
     ) {
         Card(
             modifier = Modifier
